7e7a0ca1f1ca36377a8afd67a59b2d631f39b699
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-28 00:47:19 +0530

    a.0.Input year print leap year or not.java

---
+++ a.0.Input year print leap year or not.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int n = 2002;
+        if(n/4==0){
+            System.out.println("Leap Year");
+        }
+        else{
+            System.out.println("Not Leap Year");
+        }
+    }
+}
+  OUTPUT : Not Leap Year
