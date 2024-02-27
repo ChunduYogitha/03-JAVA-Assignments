deb67fb19cf3d97497297470f14f32a08a58a291
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-02-28 01:12:12 +0530

    Sum of two numbers.java

---
+++ Sum of two numbers.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1+num2;
+        System.out.println("Sum = "+sum);
+    }
+}
+INPUT : 20 30
+OUTPUT : 50
