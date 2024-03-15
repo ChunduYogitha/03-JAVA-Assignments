5c867f7ae8ee81edc29761393fa091e2a820e99e
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-15 11:55:41 +0530

    a.2.Multiplication of table.java

--- a.2.Multiplication of table.java
+++ a.2.Multiplication of table.java
@@ -4,9 +4,7 @@ class Main{
         int n = 2;
         for(int i=0;i<10;i++){
             System.out.println(n +" * " +(i+1) +" = " +(n*(i+1)));
-        }
-        
-        
+        }      
     }
 }
 
