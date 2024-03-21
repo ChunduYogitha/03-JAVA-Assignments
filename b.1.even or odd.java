156036a8b141669259df2efb35f8f165a51534cf
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-21 12:44:40 +0530

    b.1.even or odd.java

--- b.1.even or odd.java
+++ b.1.even or odd.java
@@ -58,3 +58,53 @@ Odd
 9
 Odd
 */
+
+print only even numbers
+    
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the limit : ");
+        int n = sc.nextInt();
+        for(int i=1;i<n;i++){
+        Even(i);
+        }
+    }
+    static void Even(int i){
+        if(i%2 == 0){
+            System.out.println(i + " is Even");
+        }
+    }
+}
+/*
+Enter the limit : 5
+2 is Even
+4 is Even
+*/
+
+print only odd numbers
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        for(int i=1;i<n;i++){
+        Even(i);
+        }
+    }
+    static void Even(int i){
+        if(i%2 != 0){
+            System.out.println(i + " is odd");
+        }
+    }
+}
+/*
+Enter the number : 6
+1 is odd
+3 is odd
+5 is odd
+*/
