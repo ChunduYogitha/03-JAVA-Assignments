20befc6ddab341b7c70ccd436c62257d1d374bc3
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-21 12:32:23 +0530

    b.1.even or odd.java

--- b.1.even or odd.java
+++ b.1.even or odd.java
@@ -25,3 +25,36 @@ Enter the number : 22
 Number is Even
 Loop is ended
 */
+
+    Using call method
+    
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the limit : ");
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+        int a = sc.nextInt();
+        OddOrEven(a);
+        }
+    }
+    static void OddOrEven(int a){
+        if(a%2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
+/*
+Enter the limit : 3
+2
+Even
+7
+Odd
+9
+Odd
+*/
