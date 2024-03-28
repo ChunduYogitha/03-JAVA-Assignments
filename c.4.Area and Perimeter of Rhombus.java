923063979f30bee3c27601b315df2810f873433e
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-28 15:49:13 +0530

    c.4.Area and Perimeter of Rhombus.java

---
+++ c.4.Area and Perimeter of Rhombus.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int d1 = 15;
+        int d2 = 10;
+        System.out.println("Area of Rhombus : "+(d1*d2)/2);
+    }
+}
+/*
+Area of Rhombus : 75
+*/
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int s = 11;
+        System.out.println("Area of Rhombus : "+s*4);
+    }
+}
+/*
+Area of Rhombus : 44
+*/
