24db828361dd25110b016f65704297996c33f81d
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-28 12:48:53 +0530

    c.1.Area and perimeter of circle.java

---
+++ c.1.Area and perimeter of circle.java
@@ -0,0 +1,23 @@
+import.java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int radius = 5;
+        double pi = 3.14;
+        System.out.println("Area of Circle = " + pi*radius*radius);
+    }
+}
+/*
+Area of Circle = 78.5
+*/
+
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int radius = 9;
+        double pi = 3.14;
+        System.out.println("Perimeter of Circle = " + 2*radius*radius);
+    }
+}
+/*
+Perimeter of Circle = 162
+*/
