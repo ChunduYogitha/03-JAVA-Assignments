360e562ad49d01d311517115dd5d2c15a3bb30a2
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-28 12:56:36 +0530

    c.2.Area and Perimeter of Rectangle.java

---
+++ c.2.Area and Perimeter of Rectangle.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int Length = 12;
+        int breadth = 6;
+        System.out.println("Area of Rectangle = " + Length*breadth);
+    }
+}
+/*
+Area of Rectangle = 72
+*/
+
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int Length = 20;
+        int width = 7;
+        System.out.println("perimeter of Rectangle = " + 2*(Length*width));
+    }
+}
+/*
+perimeter of Rectangle = 280
+*/
