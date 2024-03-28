92247b0b26920fe4ec9b6503ea05639154195815
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-03-28 13:34:59 +0530

    c.3.Area and Perimeter of Parallelogram.java

---
+++ c.3.Area and Perimeter of Parallelogram.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int base = 5;
+        int height = 10;
+        System.out.println("Area of Parallelogram : " + base*height);
+    }
+}
+/*
+Area of Parallelogram : 50
+*/
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+        int a = 15;
+        int b = 10;
+        System.out.println("Perimeter of Parallelogram : " + (2*a)+(2*b));
+    }
+}
+/*
+Perimeter of Parallelogram : 3020
+*/
