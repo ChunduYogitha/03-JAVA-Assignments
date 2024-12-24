dc3248462e2d9ece024388431a1fa6d5f2dfa506
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-12-24 09:18:28 +0530

    01.evendigitsum.java

--- 01.evendigitsum.java
+++ 01.evendigitsum.java
@@ -1,9 +1,11 @@
-1. Sum of even numbers in a digit
+1) Sum of even numbers in a digit
+2) Count number of even numbers in a digit
+
 
 
 
 --------------------------------------------------------------------------------------------------------------------------
-1. sum of even numbers in a digit
+1) sum of even numbers in a digit
 
 import java.util.*;
 public class Main{
@@ -27,8 +29,60 @@ public class Main{
 
 
 //output
-Input : 123
-output : 2
+Input :
+         Enter the digit : 123
+output : 
+        2
 
   
 --------------------------------------------------------------------------------------------------------------------------
+2) Count number of even numbers in a digit
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the digit : ");
+        int n = sc.nextInt(); // 123
+        int M = n;
+        int count = 0;
+        while(n>0){
+            int rem = n%10; // 3  2   1
+            if(rem%2==0){
+                count++; // count = count+1
+            }
+            n = n/10; // 12  1  0
+        }
+        System.out.println("The count of Even numder " + M+ " is : "+count);
+    }
+}
+
+/*12456
+rem = 6   5   4.  2. 1
+6 even   4.   2
+1  2. 3
+n/10  1245.  124.   12   1. 0
+
+
+*/
+
+
+
+Input : 
+        Enter the digit : 123456
+output:
+        The count of Even numder 123456 is : 3
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    
